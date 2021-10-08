3a62a358b78bfca0d60371e013d335f01972c5c9
tlcodetribe <thabisol816@gmail.com>
2021-10-08 12:53:25 +0200

    Stroke

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,2 @@
-/**
-    * @description      : 
-    * @author           : MLab
-    * @group            : 
-    * @created          : 06/10/2021 - 11:51:25
-    * 
-    * MODIFICATION LOG
-    * - Version         : 1.0.0
-    * - Date            : 06/10/2021
-    * - Author          : MLab
-    * - Modification    : 
-**/
-import Button from './button'
-export {alertNote} from './Alert'
-export {Button}
+// export {alertNote} from './Alert'
+export {AlertNote} from './Alert'
