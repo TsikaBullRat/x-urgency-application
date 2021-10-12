32b99a6198557858b77bfd4ca9eb8d91d238af4b
TsikaBullRat <tsika1996190514@gmail.com>
2021-10-12 15:54:05 +0200

    Proper SignIn

--- src/firebase/Auth/SignIn.function.js
+++ src/firebase/Auth/SignIn.function.js
@@ -1,25 +1,13 @@
 import { auth } from "../config"
 // import { alertNote } from "../../Components"
 
-<<<<<<< HEAD
 const handleSignIn = (email, password, setMessage, setDone) =>{
     auth.signInWithEmailAndPassword(email, password)
         .then(()=>{
             setDone(true)
         })
-        .catch(err=>{
-            setDone(false)
-            switch(err.code){
-=======
-const handleSignIn = (email, password, setMessage) => {
-    auth.signInWithEmailAndPassword(email, password)
-        .then(() => {
-            // alertNote()
-            // alert("Login Successful")
-        })
         .catch(err => {
             switch (err.code) {
->>>>>>> 81d80e685c5ef885c3d2f9613cea4c66386f382d
                 case "auth/wrong-password":
                     setMessage("Your password is incorrect")
                     break;
