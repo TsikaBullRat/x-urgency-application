94ecd36072eba3295f0d39934ccb283b8647917d
tlcodetribe <thabisol816@gmail.com>
2021-10-20 10:04:48 +0200

    merged

--- src/components/index.js
+++ src/components/index.js
@@ -1,18 +1,2 @@
-/**
-    * @description      : 
-    * @author           : TLeeuw
-    * @group            : 
-    * @created          : 13/10/2021 - 11:03:51
-    * 
-    * MODIFICATION LOG
-    * - Version         : 1.0.0
-    * - Date            : 13/10/2021
-    * - Author          : TLeeuw
-    * - Modification    : 
-
-**/
-
-
 export {AlertNote} from "./Alert"
 export {HeartLoader} from "./HeartLoader"
-export {BottomNav} from "./BottomNav"
