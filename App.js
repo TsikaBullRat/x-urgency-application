098ab1bfbd5e0214363998930f820bb578a8f0e3
tlcodetribe <thabisol816@gmail.com>
2021-11-08 11:20:04 +0200

    pushing changes

--- App.js
+++ App.js
@@ -16,9 +16,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 // You can import from local files
 import { SignIn, SignUp, Home, ForgotPassword, DoctorSignUp, MedicalHome, UploadVideo, PlayVideo, DocProfile, DocSignUp, Doctor } from './src/Screens';
-import { auth } from './src/firebase'
+import { auth, LoadSet } from './src/firebase'
 import { ActivityIndicator } from 'react-native-paper';
-import { LoadSet } from './src/firebase';
 
 const Stack = createNativeStackNavigator();
 
@@ -26,7 +25,7 @@ export default function App() {
   const [successful, setSuccess] = useState(false),
     [user, setUser] = useState(''),
     [load, setLoad] = useState('');
-  LoadSet(setLoad)
+  //LoadSet(setLoad)
   
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
