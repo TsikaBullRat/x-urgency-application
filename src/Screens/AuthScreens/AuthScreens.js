0934a3467e634317a0ad7092e5d3f381ba314206
Lindiwe Mpondo <lindiwe.mpondo@gmail.com>
2022-02-03 07:55:04 +0200

    new changes

--- src/Screens/AuthScreens/AuthScreens.js
+++ src/Screens/AuthScreens/AuthScreens.js
@@ -19,7 +19,7 @@ const Stack = createNativeStackNavigator()
 
 export const AuthScreens = () => {
   return (
-    <Stack.Navigator initialRouteName='Reset Password'>
+    <Stack.Navigator initialRouteName='Sign In'>
       <Stack.Screen
         name='Doctor SignIn'
         component={MedSignIn}
