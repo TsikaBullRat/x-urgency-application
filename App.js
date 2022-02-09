c634d33a1ee0caf13ebaaa19e93c99e1e79207d4
Lindiwe Mpondo <lindiwe.mpondo@gmail.com>
2022-02-09 16:45:23 +0200

    new changes

--- App.js
+++ App.js
@@ -63,8 +63,6 @@ export default function App() {
     <NavigationContainer>
       <KeyboardAwareScrollView>
         <Stack.Navigator>
-
-        
            
           {check1 ? (
             id ? (
