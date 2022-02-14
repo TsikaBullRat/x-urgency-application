167f851c4316405c0de67009b6f1142ed7ebbcb7
Lindiwe Mpondo <lindiwe.mpondo@gmail.com>
2022-02-14 13:52:48 +0200

    new changes

--- App.js
+++ App.js
@@ -52,8 +52,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <KeyboardAwareScrollView>
-        <UserScreens />
-        <Stack.Navigator>
+        <UserScreens/>
+        {/* <Stack.Navigator>
           {check1 ? (
             id ? (
               check2 ? (
@@ -71,7 +71,7 @@ export default function App() {
           ) : (
             <Stack.Screen name="loading" component={Loading} options={{ headerShown: false }} />
           )}
-        </Stack.Navigator>
+        </Stack.Navigator> */}
       </KeyboardAwareScrollView>
     </NavigationContainer>
   );
